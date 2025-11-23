ad4a2eff75c026aef92b7d3e951247db1437119c
tbuelens <168972076+tbuelens@users.noreply.github.com>
2025-11-23 07:06:27 +0100

    Set base path for QR Code Generator

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/QR-Code-Generator/',
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
 })
